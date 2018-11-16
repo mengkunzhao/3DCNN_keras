ffdcc1c81112f4c1008e47cfcbfd4131bdbc64c9
lDark-Moonl <eunomya@gmail.com>
2018-11-16 16:24:32 +0100

    resolving model load via jason file

--- convertcaf.py
+++ convertcaf.py
@@ -8,7 +8,7 @@ def get_model(summary=False):
     """
     model = Sequential()
     # 1st layer group
-    model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', input_shape=(3, 16, 112, 112), padding='same',
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', input_shape=(112,112,16,3), padding='same',
                      name='conv1', strides=(1, 1, 1)))
     #  input_shape=(3, 16, 112, 112)))
     model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2), padding='valid', name='pool1'))
