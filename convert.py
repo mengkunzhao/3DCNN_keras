c6f67a28f82b000a9501b5e4a8452fa7737c9025
lDark-Moonl <eunomya@gmail.com>
2018-11-16 21:55:33 +0100

    conversion of theano kernels

---
+++ convert.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.layers import (Activation, Convo3D, Dense, Dropout, Flatten,
+                          MaxPooling3D,ZeroPadding3D)
+from keras.layers.advanced_activations import LeakyReLU
+from keras.losses import categorical_crossentropy
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.utils.vis_utils import plot_model
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import videoto3d
+from tqdm import tqdm
+import keras
+from keras.models import model_from_json
+import h5py
+
+
+model = Sequential()
+# 1st layer group
+model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', input_shape=(3,16,112,112), padding='same',
+                 name='conv1', strides=(1, 1, 1)))
+#  input_shape=(3, 16, 112, 112)))
+model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2), padding='valid', name='pool1'))
+
+# 2nd layer group
+model.add(Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv2', strides=(1, 1, 1)))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 2, 2), padding='valid', name='pool2'))
+
+# 3rd layer group
+
+model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv3a', strides=(1, 1, 1)))
+model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv3b', strides=(1, 1, 1)))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool3'))
+
+# 4th layer group
+model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv4a', strides=(1, 1, 1)))
+model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv4b', strides=(1, 1, 1)))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool4'))
+
+# 5th layer group
+model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv5a', strides=(1, 1, 1)))
+model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv5b', strides=(1, 1, 1)))
+model.add(ZeroPadding3D(padding=(0, 1, 1)))
+model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool5'))
+model.add(Flatten())
+
+# FC layers group
+model.add(Dense(4096, activation='relu', name='fc6'))
+model.add(Dropout(.5))
+model.add(Dense(4096, activation='relu', name='fc7'))
+model.add(Dropout(.5))
+model.add(Dense(487, activation='softmax', name='fc8'))
+print(model.summary())
+
+model.load_weights('caffe_weights/sports1M_weights.h5')
