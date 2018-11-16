f01e252c1b1ac6b00114c0e29054f35cec0f94d6
lDark-Moonl <eunomya@gmail.com>
2018-11-16 12:01:08 +0100

    inspecting problem

--- videoto3d.py
+++ videoto3d.py
@@ -21,7 +21,7 @@ class Videoto3D:
         for i in range(self.depth):
             cap.set(cv2.CAP_PROP_POS_FRAMES, frames[i])
             ret, frame = cap.read()
-            frame = cv2.resize(frame, (self.height, self.width))
+            frame = cv2.resize(frame, (112, 112))
             print(frame.shape)
             if color:
                 framearray.append(frame)
