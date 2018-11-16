d2094a9c8adec79874afc01f21f132120198cf03
lDark-Moonl <eunomya@gmail.com>
2018-11-16 22:19:05 +0100

    conversion of theano kernels

--- convert.py
+++ convert.py
@@ -25,9 +25,9 @@ import h5py
 filename = "caffe_weights/sports1M_weights.h5"
 
 h5 = h5py.File(filename,'r')
-print(h5)
-futures_data = h5['futures_data']  # VSTOXX futures data
-options_data = h5['options_data']  # VSTOXX call option data
+print(h5.keys())
+#futures_data = h5['futures_data']  # VSTOXX futures data
+#options_data = h5['options_data']  # VSTOXX call option data
 
 h5.close()
 
