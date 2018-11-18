563fb6ee6125019d53c43724b10b1c548f745f3b
lDark-Moonl <eunomya@gmail.com>
2018-11-18 18:21:44 +0100

    fix

--- 3dcnn_ensemble.py
+++ 3dcnn_ensemble.py
@@ -72,8 +72,8 @@ def loaddata(video_list, vid3d, nclass, result_dir, skip=True):
         name=os.path.join(dir, rows.split(' ')[0])
         print(name)
         temp , toload = vid3d.video3d(name, skip=skip)
-        print(temp)
-        
+        print(toload)
+
         X.append(temp)
         if toload.split('/')[-1] == rows.split(' ')[0].split('/')[-1]:
             if rows == '.DS_Store':
