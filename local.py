035fa2ec0ce5c89550c99e55636ca46f63dad0c6
lDark-Moonl <eunomya@gmail.com>
2018-11-20 21:05:59 +0100

    network config

--- local.py
+++ local.py
@@ -88,20 +88,24 @@ def loaddata(video_list, vid3d, nclass, result_dir, skip=True):
 def create_3dcnn(input_shape, nb_classes):
     model = Sequential()
     # 1st layer group
-    model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', input_shape=(input_shape), padding='same',
-                     name='conv1', strides=(1, 1, 1)))
+    model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='relu', input_shape=(input_shape), padding='same',
+                     name='conv1'))
     #  input_shape=(3, 16, 112, 112)))
-    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool1'))
+    #model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool1'))
 
     # 2nd layer group
-    model.add(Conv3D(128, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv2', strides=(1, 1, 1)))
-    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 2, 2), padding='valid', name='pool2'))
+    model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='softmax', padding='same', name='conv2'))
+    model.add(MaxPooling3D(pool_size=(3, 3, 3), strides=(2, 2, 2), padding='valid', name='pool2'))
+    model.add(Dropout(0.25))
 
     # 3rd layer group
 
-    model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv3a', strides=(1, 1, 1)))
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv3a'))
+    model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='softmax', padding='same', name='conv3b'))
+
     #model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv3b', strides=(1, 1, 1)))
-    model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2), padding='valid', name='pool3'))
+    model.add(MaxPooling3D(pool_size=(3, 3, 3), strides=(2, 2, 2), padding='valid', name='pool3'))
+    model.add(Dropout(0.25))
 
     # 4th layer group
     #model.add(Conv3D(512, kernel_size=(3, 3, 3), activation='relu', padding='same', name='conv4a', strides=(1, 1, 1)))
@@ -116,9 +120,9 @@ def create_3dcnn(input_shape, nb_classes):
     model.add(Flatten())
 
     # FC layers group
-    model.add(Dense(4096, activation='relu', name='fc6'))
+    model.add(Dense(512, activation='sigmoid', name='fc6'))
     model.add(Dropout(.5))
-    model.add(Dense(4096, activation='relu', name='fc7'))
+    model.add(Dense(512, activation='sigmoid', name='fc7'))
     model.add(Dropout(.5))
     model.add(Dense(nb_classes, activation='softmax', name='fc8'))
     print(model.summary())
