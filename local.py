0eaba544fe65ab0df2239af8ccb9af61b185aed2
lDark-Moonl <eunomya@gmail.com>
2018-11-20 20:21:25 +0100

    network config

--- local.py
+++ local.py
@@ -164,6 +164,7 @@ def main():
         Y= np_utils.to_categorical(Y_, nb_classes)
         X = X_.reshape((X_.shape[0], img_rows, img_cols, frames, channel))
         print('X_shape:{}\nY_shape:{}'.format(X.shape, Y.shape))
+        break
         X_train, X_test, Y_train, Y_test = train_test_split(
             X, Y, test_size=0.2, random_state=4)
         for i in range(args.nmodel):
