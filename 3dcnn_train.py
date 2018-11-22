44574c96f4d4f6566ecab2ba285ab9fd71baacaf
lDark-Moonl <eunomya@gmail.com>
2018-11-22 11:04:54 +0100

    minor

--- 3dcnn_train.py
+++ 3dcnn_train.py
@@ -90,7 +90,9 @@ def loaddata(video_list, vid3d, skip=True):
         if temp.shape[0] == 16:
             X.append(temp)
             label = rows.split(' ')[2]
+            print(label)
             labels.append(label.split('\n')[0])
+            print(labels)
 # The original labels start from one, but our system needs them to start from 0
     label_ = np.asarray(labels,dtype=int) - 1
     print(label_)
