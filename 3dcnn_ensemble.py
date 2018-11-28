c781e6a8a788405cdab40249b3d4a10fcf9d4683
lDark-Moonl <eunomya@gmail.com>
2018-11-28 10:00:49 +0100

    bug fixes

--- 3dcnn_ensemble.py
+++ 3dcnn_ensemble.py
@@ -193,7 +193,7 @@ def main():
     X_train_c, X_test_c, Y_train_c, Y_test_c= Xtc, Xvc, Ytc, Yvc
     X_train_d, X_test_d, Y_train_d, Y_test_d= Xtd, Xvd, Ytd, Yvd
 
-    input_color = Input(shape=X_train_c.shape[1:], dtype='float32', name='input_color')
+    input_color = Input(shape=(32,32,16,3), dtype='float32', name='input_color')
     print(input_color)
     input_depth = Input(shape=X_train_d.shape[1:], dtype='float32', name='input_depth')
     x_1 = Conv3D(32, kernel_size=(3, 3, 3), padding="same", activation=LeakyReLU)(input_color)
