a9e8b8fbc3e977054624d97ecb42d100fd6368be
lDark-Moonl <eunomya@gmail.com>
2018-11-28 10:44:44 +0100

    bug fixes

--- 3dcnn_ensemble.py
+++ 3dcnn_ensemble.py
@@ -256,7 +256,7 @@ def main():
                        optimizer=adam, metrics=['accuracy'])
     callbacks_list = [XTensorBoard('logs/{}'.format(time()))]
 
-    history = model.fit( [np.array(X_train_c), np.array(X_train_d)], y =Y_train_c, batch_size=args.batch,nb_epoch=args.epoch, verbose=1, shuffle=True , callbacks=callbacks_list)
+    history = model.fit([np.array(X_train_c), np.array(X_train_d)], Y_train_c, batch_size=args.batch,nb_epoch=args.epoch, verbose=1, shuffle=True , callbacks=callbacks_list)
 
     model_json=model.to_json()
     with open(os.path.join(args.output, 'Chalearn_3dcnnmodel_ensemble.json'), 'w') as json_file:
